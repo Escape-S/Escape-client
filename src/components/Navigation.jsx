c4b711ba993dd39cd8585aaef26fceede0a8e7b2
qlqnf16 <lee.jeongmin1027@likelion.org>
2021-10-01 00:11:31 +0900

    :sparkles: 내브바 마크업, CSS 추가

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,17 +2,20 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/reservation">Reservation</Link>
-      </li>
-      <li>
-        <Link to="/signin">Signin</Link>
-      </li>
-    </ul>
+    <div className="nav">
+      <div className="nav_logo">S-Escape</div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/reservation">Reservation</Link>
+        </li>
+        <li>
+          <Link to="/signin">Signin</Link>
+        </li>
+      </ul>
+    </div>
   );
 };
 
